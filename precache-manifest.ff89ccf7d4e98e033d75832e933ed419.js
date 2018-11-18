c63c31c9e1665d48c5b48ca37e7d672c48337628
Hikari KUBOTA <hm12017@student.miyazaki-u.ac.jp>
2018-11-18 19:20:50 +0900

    Updates

---
+++ precache-manifest.ff89ccf7d4e98e033d75832e933ed419.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9ec5ec7654130933689",
+    "url": "/wandering-pincode/static/css/main.dd23f76a.chunk.css"
+  },
+  {
+    "revision": "c9ec5ec7654130933689",
+    "url": "/wandering-pincode/static/js/main.c9ec5ec7.chunk.js"
+  },
+  {
+    "revision": "d0bca3e9d54c9e28ce37",
+    "url": "/wandering-pincode/static/js/1.d0bca3e9.chunk.js"
+  },
+  {
+    "revision": "77d93969f3e28cf8e057",
+    "url": "/wandering-pincode/static/js/runtime~main.77d93969.js"
+  },
+  {
+    "revision": "50f783eb7adf892fcb37e31c1d488cde",
+    "url": "/wandering-pincode/index.html"
+  }
+];
